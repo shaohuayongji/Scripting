b5317f7dbee4c229ded2ca3c050e641f37d17624
shaohuayongji <35660007+shaohuayongji@users.noreply.github.com>
2019-10-27 23:21:12 +0800

    Update keke.vip.js

--- keke.vip.js
+++ keke.vip.js
@@ -1,4 +1,10 @@
-let obj = JSON.parse($response.body);
-obj.Data.is_vip = 1;
-obj.Data.end_time = 1666857999;
-$done({body: JSON.stringify(obj)});
+var body = $response.body;
+var url = $request.url;
+const path1 = "/keke/mobile/index.php";
+if (url.indexOf(path1) != -1) {
+	let obj = JSON.parse(body);
+	obj.Data.is_vip = "1";
+        obj.Data.end_time = "1666857999";
+	body = JSON.stringify(obj);
+ }
+$done({body});
