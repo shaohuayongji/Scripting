f542d5d3e9845d4f7f011f5dc13dbbe41dc1bf91
shaohuayongji <35660007+shaohuayongji@users.noreply.github.com>
2019-10-27 23:13:55 +0800

    Update keke.vip.js

--- keke.vip.js
+++ keke.vip.js
@@ -1,4 +1,4 @@
 let obj = JSON.parse($response.body);
-obj.data.is_vip = 1;
-obj.data.end_time = 1666857999;
+obj.Data.is_vip = 1;
+obj.Data.end_time = 1666857999;
 $done({body: JSON.stringify(obj)});
